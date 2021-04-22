88035bdcdc8812f8e7a9820e5f23bdeea5fc5871
Eric Chen <airkro@qq.com>
2021-04-22 15:41:10 +0800

    fix: remove postinstall hook

--- index.cjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const Cheetor = require('cheetor');
-
-const { builder } = require('./lib/builder.cjs');
-const { action } = require('./lib/action.cjs');
-
-new Cheetor().config(builder).setup(action);
