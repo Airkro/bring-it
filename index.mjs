63589a38d4d3acbabd2b70c28d4724ae67b85394
Eric Chen <airkro@qq.com>
2021-07-14 15:42:03 +0800

    refactor: logic

--- index.mjs
+++ index.mjs
@@ -1,62 +1,18 @@
 import { pathToFileURL } from 'url';
 
-import { NodeSSH } from 'node-ssh';
-import slash from 'slash';
+import { logger } from './lib/logger.mjs';
+import { checkSource } from './lib/prepare.mjs';
+import { SSH } from './lib/ssh.mjs';
+import { upload } from './lib/upload.mjs';
 
-import { uploadAll } from './lib/upload.mjs';
-import { checkSource, logger } from './lib/utils.mjs';
-
-export function action({
-  cwd,
-  key,
-  server: { user, hostname, port, path: dir },
-}) {
+export function action({ cwd, key, server: { user, hostname, port, path } }) {
   if (checkSource(cwd)) {
-    const ssh = new NodeSSH();
+    logger.info('From:', pathToFileURL(cwd).toString());
+    logger.info('To:', `sftp://${user}@${hostname}:${port}/${path}`);
 
-    ssh
-      .connect({
-        host: hostname,
-        port,
-        username: user,
-        privateKey: key,
-        tryKeyboard: false,
-      })
-      .then(() => {
-        logger.info('From:', pathToFileURL(cwd).toString());
-        logger.info(
-          'To:',
-          new URL(slash(dir), `sftp://${user}@${hostname}:${port}`).toString(),
-        );
+    const options = { hostname, port, user, key };
 
-        logger.info('Connection:', 'start');
-
-        ssh.connection.on('error', (error) => {
-          process.exitCode = 1;
-          logger.fail(error.message);
-          ssh.dispose();
-        });
-
-        ssh.connection.on('end', () => {
-          logger.info('Connection:', 'end');
-        });
-
-        ssh.connection.on('close', () => {
-          logger.info('Connection:', 'close');
-          // eslint-disable-next-line unicorn/no-process-exit
-          process.exit();
-        });
-      })
-      .then(() => uploadAll(ssh, cwd, dir))
-      .catch((error) => {
-        process.exitCode = 1;
-        setTimeout(() => {
-          logger.fail(error.message);
-        }, 0);
-      })
-      .finally(() => {
-        ssh.dispose();
-      });
+    SSH(options, (ssh) => upload(ssh, cwd, path));
   }
 }
 
