3e0f7a4d7a1658adee483542b34d733a2459bd65
Eric <airkro@qq.com>
2022-03-22 15:16:29 +0800

    fix: bug

--- bin.mjs
+++ bin.mjs
@@ -7,8 +7,9 @@ import { Cheetor } from 'cheetor';
 import * as pack from './lib/cmd/pack.mjs';
 import * as sftp from './lib/cmd/sftp.mjs';
 
-const raw = readFileSync('./package.json');
-
+const current = import.meta.url;
+const file = new URL('package.json', current);
+const raw = readFileSync(file);
 const pkg = JSON.parse(raw);
 
 new Cheetor(pkg, import.meta.url)
